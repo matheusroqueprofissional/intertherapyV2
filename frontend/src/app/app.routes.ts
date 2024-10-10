0c9421aa7b4da664c1afae5859ff84c4dcdfefcc
Matheus Brito <matheus.brito@eldorado.org.br>
2024-10-10 12:32:55 -0300

    header almost finished, need fix the gap betwen links and image

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ServicesComponent } from './pages/services/services.component';
+import { TeamComponent } from './pages/team/team.component';
 
 export const routes: Routes = [
-
-  {path:"",component:DashboardComponent},
-  {path:"contact",component:ContactComponent},
-  {path:"about",component:AboutComponent},
-  {path:"services",component:ServicesComponent}
+  { path: 'inicio', redirectTo: '' },
+  { path: '', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'contact', component: ContactComponent },
 ];
--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'], // Corrigi a propriedade 'styleUrl' para 'styleUrls'
 })
 export class HeaderComponent {
+  links = ['inicio', 'about', 'services', 'team', 'contact'];
+  hoverColors = ['#A8CF45', '#E50612', '#2DABE5', '#F58634', '#090909'];
+  isHovered = [false, false, false, false, false]; // Estado de hover de cada link
 
+  constructor(private router: Router) {}
+
+  redirectTo(value: string) {
+    console.log(value);
+    this.router.navigate([value]);
+  }
 }
