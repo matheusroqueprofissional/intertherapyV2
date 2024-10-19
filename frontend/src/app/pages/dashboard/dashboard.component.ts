137f1527f7b71490206ae5011a193a686f2d99d8
Matheus Brito <matheus.brito@eldorado.org.br>
2024-10-19 05:17:50 -0300

    added vlibras

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+import { AngularVlibras } from 'angular-vlibras';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [AngularVlibras],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
