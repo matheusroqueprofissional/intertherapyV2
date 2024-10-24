9583bb7893b1fe4818bd5e7438cd3ca0bbc5ebc9
Matheus Brito <matheus.brito@eldorado.org.br>
2024-10-24 17:29:31 -0300

    fixes done on footer component and in vlibras extension

--- frontend/src/app/shared/footer/footer.component.ts
+++ frontend/src/app/shared/footer/footer.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
-  templateUrl: './about.component.html',
 })
 export class FooterComponent {
 
