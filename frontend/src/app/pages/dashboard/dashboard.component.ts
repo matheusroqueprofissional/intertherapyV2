cc537417e9b79e1f22a60bf6cde03d5da4ac5c00
Matheus Brito <matheus.brito@eldorado.org.br>
2024-11-19 11:56:58 -0300

    i18n implemented

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,28 +1,31 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
-import { TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateModuleModule } from '../../translate-module/translate-module.module';
-// Fábrica do Loader para tradução
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json'); // Ajuste o caminho conforme necessário
-}
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     MatIcon,
-    TranslateModuleModule, // Sem o `.forRoot` aqui
+    TranslateModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'], // Corrigido o nome da propriedade
 })
 export class DashboardComponent {
-  constructor(private translate: TranslateService) {
-    // Definir o idioma padrão
-    this.translate.setDefaultLang('english'); // Nome do arquivo JSON sem a extensão
-    this.translate.use('english'); // Idioma ativo
+  constructor(private translateService: TranslateService) {
+
+    const userLang = navigator.language || 'en';
+    const languageMap: { [key: string]: string } = {
+      'en': 'EN-US',
+      'pt': 'PT-BR',
+    };
+
+    const languageCode = userLang.split('-')[0]; // Pega apenas o código base do idioma
+    const mappedLang = languageMap[languageCode] || 'EN-US'; // Usa EN-US como fallback
+
+    this.translateService.setDefaultLang(mappedLang);
+    this.translateService.use(mappedLang);
   }
 }
--- frontend/src/app/translate-module/translate-module.module.ts
+++ frontend/src/app/translate-module/translate-module.module.ts
@@ -1,19 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import {
+  TranslateLoader,
+  TranslateModule,
+} from '@ngx-translate/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, '../../assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
+  providers:[
+  ],
   imports: [
+    BrowserModule,
     CommonModule,
+    HttpClientModule,
     TranslateModule.forRoot({
-                loader: {
-                  provide: TranslateLoader,
-                  useClass: TranslateFakeLoader
-                }
-              })
-  ]
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps:[HttpClient]
+      },
+    }),
+  ],
 })
-export class TranslateModuleModule { }
+export class TranslateModuleModule {}
