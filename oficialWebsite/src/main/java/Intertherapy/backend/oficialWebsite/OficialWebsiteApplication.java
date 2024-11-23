06d7ab65b9a8968eab784f377e8873951fd9f8a1
Matheus Brito <matheus.brito@eldorado.org.br>
2024-11-23 19:00:13 -0300

    mais backend

---
+++ oficialWebsite/src/main/java/Intertherapy/backend/oficialWebsite/OficialWebsiteApplication.java
@@ -0,0 +1,13 @@
+package Intertherapy.backend.oficialWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficialWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficialWebsiteApplication.class, args);
+	}
+
+}
