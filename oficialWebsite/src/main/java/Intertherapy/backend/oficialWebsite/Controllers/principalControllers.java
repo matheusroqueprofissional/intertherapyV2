1d5a3674c81a8a86e52b8bb6751dca6a9dcf0edd
Matheus Brito <matheus.brito@eldorado.org.br>
2024-11-23 18:59:38 -0300

    mais backend

--- oficialWebsite/src/main/java/Intertherapy/backend/oficialWebsite/Controllers/principalControllers.java
+++ oficialWebsite/src/main/java/Intertherapy/backend/oficialWebsite/Controllers/principalControllers.java
@@ -1,4 +1,14 @@
 package Intertherapy.backend.oficialWebsite.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class principalControllers {
+    @GetMapping("teste")
+    public String teste() {
+        return "funfo";
+    }
 }
