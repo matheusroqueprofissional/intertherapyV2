8b4c9afccf87de83cc99158f74d970b042120d6a
Matheus Brito <matheus.brito@eldorado.org.br>
2024-11-24 02:43:26 -0300

    bug fixed

--- oficialWebsite/src/main/java/Intertherapy/backend/oficialWebsite/Controllers/principalControllers.java
+++ oficialWebsite/src/main/java/Intertherapy/backend/oficialWebsite/Controllers/principalControllers.java
@@ -3,24 +3,20 @@ package Intertherapy.backend.oficialWebsite.Controllers;
 import Intertherapy.backend.oficialWebsite.model.Treatment;
 import Intertherapy.backend.oficialWebsite.service.TreatmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping
+@RestController
+@RequestMapping("/system")
 public class principalControllers {
 
     @Autowired
     TreatmentService treatmentService;
 
-    @GetMapping("teste")
+    @GetMapping("/teste")
     public String teste() {
         return "funfo";
     }
 
-    @PostMapping
+    @PostMapping("/saveTreatments")
     public Treatment saveTreatment(@RequestBody Treatment treatment) {return treatmentService.create(treatment);}
 }
