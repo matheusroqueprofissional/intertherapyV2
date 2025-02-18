aab85f233f00c999348693723e58fec0f4854415
Matheus Brito <matheus.brito@eldorado.org.br>
2025-02-18 13:50:07 -0300

    codigo atualizado

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { NgFor } from '@angular/common';
+import { CarouselComponent, CarouselInnerComponent, CarouselItemComponent, ThemeDirective } from '@coreui/angular';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +11,9 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
   imports: [
     MatIcon,
     TranslateModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    ThemeDirective, CarouselComponent, CarouselInnerComponent, NgFor, CarouselItemComponent
+
   ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
@@ -19,5 +23,22 @@ export class DashboardComponent {
   imageSrc = '../../../assets/images/dashboard/dashboardKid.svg';
 
 
+  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: './assets/img/angular.jpg'
+    };
+    this.slides[1] = {
+      src: './assets/img/react.jpg'
+    };
+    this.slides[2] = {
+      src: './assets/img/vue.jpg'
+    };
+  }
+
+  onItemChange($event: any): void {
+    console.log('Carousel onItemChange', $event);
+  }
 
 }
